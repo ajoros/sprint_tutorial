9d8e451e2460e534af4d24115dc510a7cf6ecd3e
ajoros <andrew.joros@gmail.com>
2017-07-15 12:01:33 -0500

    Added Hello World with two numbers printed

--- sprint_tutorial/compute.py
+++ sprint_tutorial/compute.py
@@ -1,6 +1,10 @@
 """ Module to compute stuff
 """
 
+def print_hello_world_with_numbers(x,y):
+    """Print hello world with two numbers you chose
+    """
+    return print('Hello World, you are using {} and {}'.format(x,y))
 
 def my_sum(x, y):
     """ Compute the sum of 2 numbers
